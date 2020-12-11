0996a8c3714816fa91ef009a9b2efbca4719203e
bowen125125 <bowen125125@gmail.com>
2020-12-11 16:31:49 +0800

    666

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,6 @@
+$("html").easeScroll();
+
+
 let nowloc = 0
     console.log($('.road').offsetHeight);
     $(window).on('scroll', function(){
@@ -19,3 +22,41 @@ let nowloc = 0
       }
     })
   $('.parallax-window').parallax({imageSrc: './images/884373.png'})
+
+  // loading
+  let loadingBar = new ldBar("#loadingBar", {
+    // 值得設定 ------------------------------------------------------------------
+    "value": 0, // 目前值
+    'min': 0, // 最小值
+    'max': 100, // 最大值
+    // 'precision': '0.00001', // 精確浮點數，小數點幾位數
+    // 'duration': 1,
+
+    // 使用套件圖形設定 ----------------------------------------------------------
+    // 'preset': 'energy', // 形狀,circle、fan、bubble、line、stripe、energy、rainbow
+    // "stroke": 'yellow', // 不支援 rgba()，其他支援
+    // "stroke-width": 2, // 粗細
+    // 'stroke-lincap': 'round',
+
+    // 使用自訂義圖片 ------------------------------------------------------------
+    'img': './images/load.svg',
+    'img-size': '120,120',
+    'type': 'fill',
+    'fill-dir': 'btt', // loading 方向，ltr、rtl、btt、ttb
+    'fill-background': '#333', // 背景填滿顏色
+    'fill-background-extrude': 1, // 背景填滿區域大小,0跟圖片一樣大
+  });
+
+  let loadingTxt = 0;
+  $('img').css({ display: 'none' })
+  $(window).on('load', function () {
+    let timer = setInterval(() => {
+      loadingBar.set(loadingTxt)
+      if (loadingTxt >= 100) {
+        $('img').fadeIn();
+        $('#loadingBar').fadeOut();
+      } else {
+        loadingTxt++
+      }
+    }, 30);
+  })
